3e6f1bb2018455974c32d7bc6474d86680ec9e5a
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-18 15:04:18 -0700

    Window dimensions

--- main/main.py
+++ main/main.py
@@ -10,7 +10,11 @@ SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
+class Snake:
+    pass
 
+class Food:
+    pass
 
 def next_turn():
     pass
@@ -37,4 +41,19 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height = GAME_HEIGHT, width = GAME_WIDTH)
 canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
 window.mainloop()
